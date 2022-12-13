3e97e0714d03055215d728139c3a8473d40ed01a
storymachinedata <120018282+storymachinedata@users.noreply.github.com>
2022-12-13 11:02:35 +0100

    Update LinkedIn_Keyword_Monitor.py

--- LinkedIn_Keyword_Monitor.py
+++ LinkedIn_Keyword_Monitor.py
@@ -34,7 +34,7 @@ with col2:
 
 st.sidebar.success("Choose Category")
 
-st.title('LinkedIn Keyword Search Monitoring')
+st.title('Dobner: LinkedIn Keyword Search Monitoring')
 
 st.image(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png",
