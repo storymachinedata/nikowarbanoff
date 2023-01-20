8443b58a4f541f0fcf6f156a1394a0543857d90b
storymachinedata <120018282+storymachinedata@users.noreply.github.com>
2023-01-20 11:43:56 +0100

    Update LinkedIn_Keyword_Monitor.py

--- LinkedIn_Keyword_Monitor.py
+++ LinkedIn_Keyword_Monitor.py
@@ -47,10 +47,7 @@ for percent_complete in range(100):
 month = datetime.today().month
 day = datetime.today().day
 
-#dobner_search_results = f'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor{month}_{day}.csv'
-
-dobner_search_results = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor1_21.csv'
-#https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor1_19.csv
+dobner_search_results = f'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor{month}_{day}.csv'
 
 						 #https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor1_18.csv
 
