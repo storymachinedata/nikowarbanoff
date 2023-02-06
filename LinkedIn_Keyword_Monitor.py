412b8f499d9be2081a1e66b6cfc74bd238f00862
storymachinedata <120018282+storymachinedata@users.noreply.github.com>
2023-02-06 09:22:48 +0100

    Update LinkedIn_Keyword_Monitor.py

--- LinkedIn_Keyword_Monitor.py
+++ LinkedIn_Keyword_Monitor.py
@@ -49,7 +49,7 @@ day = datetime.today().day
 
 #dobner_search_results = f'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor{month}_{day}.csv'
 
-dobner_search_results = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/HqI9Zcqdu5EZ6aqaXrXumg/nikowarbanoff_keywordSearchMonitor2_2.csv'
+dobner_search_results = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/HqI9Zcqdu5EZ6aqaXrXumg/nikowarbanoff_keywordSearchMonitor2_6.csv'
 
 						 #https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/WVWDm0XnEmqgQ4iNu89Rkg/dobner_keywordSearchMonitor1_18.csv
 
